369f34357e3ea7280c651a490628cf8a6b525233
Gourav Nagpal <105379848+gouravnagpal@users.noreply.github.com>
2026-02-13 11:17:02 +0530

    SEO updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import Response from "@/utils/response";
 import { Fragment } from "react";
+import SEO from "@/meta/seo";
 
 export default function App({ Component, pageProps }) {
 	return (
 		<Fragment>
+			<SEO />
 			<Component {...pageProps} />
 			<Response></Response>
 		</Fragment>
